9356822cd858370068c90c99220ffd9c94317adf
Matheus Silva <souza.matheus@unifesp.br>
2023-07-21 01:56:36 -0300

    Finish project ✅

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=+dr#+9rc8&6#ax5#zlzd01ex6n&-=!i662_st^d8$i6ptfx!h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesomefree',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import date, timedelta
+import plotly.graph_objects as go
+
+def index(request):
+    one_year_ago = date.today() - timedelta(days=365)
+    current_date = date.today()
+
+    context = {
+        'one_year_ago': one_year_ago,
+        'current_date': current_date,
+    }
+    return render(request, 'core/index.html', context)
